4beab061285c1f11e90050eb428c9be0fd2beebe
nitinjmv <nitinjmv@gmail.com>
2024-07-24 19:03:21 +0100

    Initial commit

---
+++ Crypto-Account-CRUD/account-crud/src/main/java/dev/jmv/account/AccountCrudApplication.java
@@ -0,0 +1,13 @@
+package dev.jmv.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountCrudApplication.class, args);
+	}
+
+}
---
+++ Crypto-Account-CRUD/crypto-api/src/test/java/dev/jmv/crypto/CryptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmv.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
